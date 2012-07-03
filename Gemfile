e059b387aab062250a12404f8f445dbb89493840
Jeremy Kemper <jeremy@bitsweat.net>
2012-07-03 15:05:36 -0700

    Update for Rails 4. Backward-compatible with 3-2-stable.

--- Gemfile
+++ Gemfile
@@ -1,2 +1,5 @@
 source "http://rubygems.org"
 gemspec
+
+gem 'rails', github: 'rails/rails'
+gem 'active_record_deprecated_finders', github: 'rails/active_record_deprecated_finders'
--- lib/action_dispatch/routing/concerns.rb
+++ lib/action_dispatch/routing/concerns.rb
@@ -20,34 +20,29 @@ end
 module ActionDispatch::Routing::Mapper::ResourcesWithConcerns
   extend ActiveSupport::Concern
 
-  included do
-    alias_method_chain :resource,  :concerns
-    alias_method_chain :resources, :concerns
-  end
-
-  def resource_with_concerns(*resources, &block)
+  def resource(*resources, &block)
     if (options_with_concerns = resources.last).is_a?(Hash)
-      named_concerns = options_with_concerns[:concerns]
+      named_concerns = options_with_concerns.delete(:concerns)
 
-      resource_without_concerns(*resources) do
+      super(*resources) do
         concerns(named_concerns)
         block.call if block_given?
       end
     else
-      resource_without_concerns(*resources, &block)
+      super(*resources, &block)
     end
   end
 
-  def resources_with_concerns(*resources, &block)
+  def resources(*resources, &block)
     if (options_with_concerns = resources.last).is_a?(Hash)
-      named_concerns = options_with_concerns[:concerns]
+      named_concerns = options_with_concerns.delete(:concerns)
 
-      resources_without_concerns(*resources) do
+      super(*resources) do
         concerns(named_concerns)
         block.call if block_given?
       end
     else
-      resources_without_concerns(*resources, &block)
+      super(*resources, &block)
     end
   end
 end
